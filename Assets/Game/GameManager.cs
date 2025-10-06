38bf9b1b2dc20c5515e13c3ea7b59fbfa1e522e5
오명현 <rubtub2314@gmail.com>
2025-10-06 15:28:06 +0900

    슬롯들 캐싱 및 ID 부여

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections;
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SerializedMonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public Dictionary<int, Slot> SlotCache { get; private set; } = new Dictionary<int, Slot>();
 
     public Observer<GameData> GameData = new Observer<GameData>();
+
+    [SerializeField] private List<Slot> slots;
 
     private void Awake()
     {
@@ -17,7 +23,28 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        Initialize();
     }
 
+    public void Initialize()
+    {
+        for (int index = 0; index < slots.Count; index++)
+        {
+            slots[index].id = index + 1;
+        }
 
+        Cache();
+    }
+
+    private void Cache()
+    {
+        SlotCache.Clear();
+
+        for (int index = 0; index < slots.Count; index++)
+        {
+            var slot = slots[index];
+            SlotCache.Add(slot.id, slot);
+        }
+    }
 }
